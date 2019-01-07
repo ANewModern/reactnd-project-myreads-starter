ecbf7c1ed5e05d3d426a27bf4ae80da219824a4e
anewmodern <jimmleb178@gmail.com>
2019-01-07 11:06:55 -0800

    Select option now highlighted, fixed shelf bug

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,17 +1,23 @@
 import React from 'react';
 
 class Book extends React.Component {
+  state = {
+    optionsState: ''
+  }
   onBookUpdate = (e, book) => {
     const target = e.target.value;
     this.props.onBookUpdate(book, target);
+  }
+  componentDidMount() {
+    this.setState({ optionsState: this.props.book.shelf ? this.props.book.shelf : 'none' });
   }
   render() {
     return (
       <div className="book">
         <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url(' + this.props.book.imageLinks.thumbnail + ')' }}></div>
+          {!!this.props.book.imageLinks.thumbnail && <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url(' + this.props.book.imageLinks.thumbnail + ')' }}></div>}
           <div className="book-shelf-changer">
-            <select onChange={(e) => this.onBookUpdate(e, this.props.book)}>
+            <select onChange={(e) => this.onBookUpdate(e, this.props.book)} value={this.state.optionsState}>
               <option value="move" disabled>Move to...</option>
               <option value="currentlyReading">Currently Reading</option>
               <option value="wantToRead">Want to Read</option>
