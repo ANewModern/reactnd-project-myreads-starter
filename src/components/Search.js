7029bbe6d3cb10b3e78d39e94cfaeeadec495848
anewmodern <jimmleb178@gmail.com>
2019-01-07 11:27:14 -0800

    Fixed the search when input is empty

--- src/components/Search.js
+++ src/components/Search.js
@@ -20,26 +20,22 @@ class Search extends React.Component {
             this.setState({ inputText });
             this.search(inputText);
         } else {
-            this.setState({ inputText });
+            this.setState(() => {
+                return {
+                    books: [],
+                    inputText
+                };
+            });
         }
     }
-    
+
     render() {
         return (
             <div className="search-books">
                 <div className="search-books-bar">
                     <Link to="/"><button className="close-search">Close</button></Link>
                     <div className="search-books-input-wrapper">
-                        {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                        <input type="text" placeholder="Search by title or author" value={this.state.inputText} onChange={(e) => this.onTextChange(e)}/>
-
+                        <input type="text" placeholder="Search by title or author" value={this.state.inputText} onChange={(e) => this.onTextChange(e)} />
                     </div>
                 </div>
                 <div className="search-books-results">
